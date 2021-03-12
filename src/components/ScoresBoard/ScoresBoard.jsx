0fd278f229b4f463ec85b78afa89b2be25e4186f
Tianyuan Deng <49263084+tdeng3@users.noreply.github.com>
2021-03-12 11:46:04 -0600

    Update ScoresBoard.jsx

--- src/components/ScoresBoard/ScoresBoard.jsx
+++ src/components/ScoresBoard/ScoresBoard.jsx
@@ -3,15 +3,15 @@ import './ScoresBoard.css';
 import axios from "axios";
 
 export default function ScoresBoard(){
-    const [scores, setScores] = useState({});
+    const [scores, setScores] = useState([]);
     
-    const players = axios.get("http://localhost:8080/score").then((resp)=>{
+    axios.get("http://localhost:8080/score").then((resp)=>{
             console.log("Scores: ",resp.data)
             setScores(resp.data)
-            console.log("useState: ", scores)
+            
         })
         
-    console.log("Scores: ", players.data);
+    
     console.log("Scores? ", scores);
     const renderScores = (player,index) => {
         return (
